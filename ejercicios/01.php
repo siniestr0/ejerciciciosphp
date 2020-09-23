80f642dff0dc95ace2d4c65bb38fb89c1407e85c
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-23 17:25:27 +0100

    ejercicio 01 hola mundo

---
+++ ejercicios/01.php
@@ -0,0 +1,21 @@
+/** 
+Creamos la estructura normal de un html CODE STUDIO LA CREA SOLO
+*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio1</title>
+</head>
+<body>
+/** 
+Dentro de body creamos la estructura de php y empezamos.
+ */
+
+    <?php 
+    $cadena = "Hola Mundo";
+    echo $cadena; 
+    ?>
+</body>
+</html>
