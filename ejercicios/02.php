5c97d20a768e360f39fad8c80a13d5a1f9dbb5ac
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 15:34:15 +0100

    tabla de multiplicar

---
+++ ejercicios/02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tabla de multiplicar</title>
+</head>
+<body>
+    <?php
+
+    define('SIZE', 10);
+        echo "<table border=1>";
+        
+        echo "</table>";
+
+    ?>
+</body>
+</html>
