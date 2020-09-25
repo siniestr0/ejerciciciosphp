36adbf19b6a00706fa2d82eb18215523a9df6040
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 18:01:32 +0100

    fibonacci

---
+++ ejercicios/04.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fibonacci</title>
+</head>
+<body>
+
+    <?php
+
+    echo "Mostrar los 10 primero números de la secuencia de fibonacci" . "<br>";
+    $valor1 = 0;
+    $valor2 = 1;
+
+    
+    ?>
+
+</body>
+</html>
