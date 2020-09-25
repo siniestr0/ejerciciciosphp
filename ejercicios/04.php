4fb2ca8a54727c6409dcb4377c6e2805a10a1630
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 18:52:25 +0100

    fibonacci

--- ejercicios/04.php
+++ ejercicios/04.php
@@ -17,7 +17,7 @@ $valor2=1;
 // fibonacci   1+1= 2 2+1= 3  3+2 = 5  3+5=8 ...   ( 1,1,2,3,5,8 ...)
 // se suma el valor anterior al valor siguiente para sacar el siguiente 
 echo $valor1 ." ";
-    while ($valor1<10){  // Aquí sacara los 10 primeros valores
+    while ($valor1<=10){  // Aquí sacara los 10 primeros valores
     
         echo $valor2 ." ";
         $valor2 += $valor1;   
