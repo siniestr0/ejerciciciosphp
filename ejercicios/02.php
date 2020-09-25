d7126d02d59dd15e4f33a9e39dd62fdfe29cfeda
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 15:58:37 +0100

    tabla de multiplicar

--- ejercicios/02.php
+++ ejercicios/02.php
@@ -9,12 +9,35 @@
     <?php
     echo "<table border = 1>";
     echo "<tr>";
-        for ($cabecera="0";$cabecera<=10;$cabecera++){
+        for ($cabecera="0";$cabecera<=10;$cabecera++){ //creamos una lista de numeros hasta el 10 dentro de una tabla
              echo "<th>";
              echo $cabecera;
              echo "</th>";
      }
     echo "</tr>";
+/*
+base sera el numero por el que despues sera multiplicado.
+Se crea con un bucle for para que llegue hasta 10, si ponemos mas, pues multipicara por mas
+*/
+    for ($base = 1; $base <= 10; $base++){
+        echo "<tr>";
+        echo "<th>";
+        echo $base;
+        echo "</th>";
+        /*
+        Ahora tenemos que crear otro bucle for para hacer la multiplicacion.
+        Segun este bucle, vamos a multiplicar base · multiplicador
+        de modo que con el bucle, irá añadiendo tantas filas como
+        multiplicadores hayamos puesto
+        */
+             for ($multiplicador =1; $multiplicador <= 10; $multiplicador++){                      
+                     $multiplicacion = $base*$multiplicador;                    
+                     echo "<td>";                      
+                     echo $multiplicacion;                      
+                     echo "</td>";              
+             }              
+     echo "</tr>";          
+     }      
     echo "</table>"
 
     ?>
