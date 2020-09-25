7dffb9f831d00d97715692e68169e3a2d626e725
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 18:21:57 +0100

    fibonacci

--- ejercicios/04.php
+++ ejercicios/04.php
@@ -10,12 +10,13 @@
     <?php
 
     echo "Mostrar los 10 primero números de la secuencia de fibonacci" . "<br>";
-    $valor1 = 1;
-    $valor2 = 0;
+    $valor1 = 0;
+    $valor2 = 1;
 
     for ($i=0; $i <= 10; $i++) { 
         }
         $suma = $valor1 + $valor2;
+        echo $suma;
         $valor1 = $valor2;
         $valor2 = $suma;
         echo $suma . " , ";
