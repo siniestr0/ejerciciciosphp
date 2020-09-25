271859f588a7a2bd6057eaf94361da5ad87d1e9b
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 17:55:29 +0100

    divisibles por 3

---
+++ ejercicios/03.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numeros divisibles por 3</title>
+</head>
+<body>
+    
+    <?php
+
+    for ($x = 1; $x <= 10; $x++) {
+
+        if ( $x % 3 == 0) {
+         echo $x . "<br>";
+        }
+    }
+
+
+    ?>
+
+</body>
+</html>
