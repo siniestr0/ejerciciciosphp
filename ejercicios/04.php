75a2fb438da95c9310804ae1345492377a4ddefa
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-25 14:10:58 +0100

    ejercicio 3-1

---
+++ ejercicios/04.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio 04</title>
+</head>
+<body>
+    <!-- Aqui en el body es donde se pondrá siempre la estructura del php-->
+
+    <?php
+
+
+    ?>
+</body>
+</html>
