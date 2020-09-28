e1e062a2910db4377a1460d749c004a2bcabb8d0
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-28 15:09:05 +0100

    divisores

---
+++ ejercicios/05.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numeros Divisibles</title>
+</head>
+<body>
+    
+<?php
+
+const num1 = 163;
+
+
+
+ 
+for($i = 1; $i < num1; $i ++) {
+        if (num1 % $i == 0) {
+            echo $i;
+        }
+}
+
+
+?>
+
+</body>
+</html>
