506059ba038e43efd86fcf6b981f03e34a5b51cb
Jose Luis Hervas <jlcruceras@gmail.com>
2020-09-28 15:21:08 +0100

    divisores

--- ejercicios/05.php
+++ ejercicios/05.php
@@ -10,17 +10,22 @@
 <?php
 
 const num1 = 169;
+const num2 = 193;
 
- 
 for($i = 1; $i < num1; $i ++) {
         if (num1 % $i == 0) {
             
             echo "num1 " . "tiene " . $i . " divisores";
-        }
-        
+        }        
 }
+echo "<br>";
 
-
+for($i = 1; $i < num2; $i ++) {
+    if (num2 % $i == 0) {
+        
+        echo "num2 " . "tiene " . $i . " divisores";
+    }        
+}
 
 ?>
 
