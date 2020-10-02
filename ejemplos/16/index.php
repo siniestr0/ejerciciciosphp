49f80ccbb90a3dda7251cb2aba41ecbd2cdda7f5
Rafa Cabeza <rafacabeza@gmail.com>
2020-10-02 08:54:52 +0200

    16

--- ejemplos/16/index.php
+++ ejemplos/16/index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ejemplos</title>
-</head>
-<body>
-  <?php
-    require('Persona.php');
-    $juan = new Persona('Juan', 'García', 15);
+<?php
 
-    echo "Hola, soy $juan";
-    echo "<br>";
-    echo "Mi nombre completo es $juan->nombre $juan->apellido y tengo $juan->edad años";
-  ?>
-</body>
-</html>
+require_once "App.php";
+$app = new App;
+$app->run();
