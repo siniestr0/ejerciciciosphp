0c70c0c7c699965b3a92aebecf459e3da1185f1f
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-02 16:03:54 +0100

    arrays ordenados

--- ejercicios/08.php
+++ ejercicios/08.php
@@ -7,9 +7,22 @@
 </head>
 <body>
     <?php
-    
+    //Creamos el array
 
-    
+    $equipo1 = [];
+    $equipo1[0]="Jose";
+    $equipo1[1]="Jordy";
+    $equipo1[2]="Elena";
+    $equipo1[3]="Dionis";
+
+    echo"<br>";
+    foreach ($equipo1 as $position => $element){
+
+        echo $position = $element ."<br>";   
+    }
+
+print_r($equipo1);
+
     ?>
 </body>
 </html>
