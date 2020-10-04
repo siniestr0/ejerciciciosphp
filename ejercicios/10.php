597ebfa63a69a2f4998afd68b30dd6262beace57
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-04 12:22:26 +0100

    add new elements Basket

--- ejercicios/10.php
+++ ejercicios/10.php
@@ -12,10 +12,9 @@
 echo "Equipo antes de añadir a otro jugador";
 
 $equipo1 = array (
-    'Jordy' => 'Pivot',
     'Jose' => 'Base',
     'Elena' => 'Escolta',
-    'Dionis' => 'Alero'
+    
 );
 
 echo "<br>";
@@ -26,6 +25,8 @@ echo "<br>";
 echo "Equipo después de añadir a otro jugador";
 
 $equipo1 ['Ramona'] = 'Ala';
+$equipo1 ['Dionis'] = 'Alero';
+
 echo "<br>";
 
 foreach ($equipo1 as $position => $element){
