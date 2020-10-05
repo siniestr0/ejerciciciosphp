a11b8a774fadd0edb6b7e7da636e888c72d7cd44
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-05 15:58:46 +0100

    form min characters

---
+++ ejercicios/13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Nombre con minimo caracteres</title>
+</head>
+<body>
+    
+<!-- Creamos el formulario -->
+
+<form action = "" method = "post">
+
+<!-- si ponemos al final del input un require, este campo será obligatorio-->
+Name: <input type = "text" name = "Name" required><br>
+
+<input type = "submit" value = "Send">
+
+</form>
+
+</body>
+</html>
