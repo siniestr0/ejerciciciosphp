53c70c3faf9e41c6c6d6f7fe6d0c4ba5347ee051
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-05 19:26:29 +0100

    views links add

--- POO/views/fibonacci.php
+++ POO/views/fibonacci.php
@@ -1,3 +1,9 @@
 <?php
 
-function fibonaci();
+echo "<pre>";
+print_r($fibonacci);
+
+?>
+<ul>
+<li> <a href="http://ejercicios.local/POO/index.php"> index </a></li>
+</ul>
---
+++ POO/views/potencias.php
@@ -0,0 +1,3 @@
+<ul>
+<li> <a href="http://ejercicios.local/POO/index.php"> index </a></li>
+</ul>
