6e84994ccafee52e609cbfe19793c3df71a73614
JoseLuisHervas <59416429+siniestr0@users.noreply.github.com>
2020-10-08 10:13:59 +0200

    fixing

--- POO/views/index.php
+++ POO/views/index.php
@@ -2,9 +2,10 @@
 <ul>
 <li> <a href="http://ejercicios.local/POO/index.php"> index </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=fibonacci"> Fibonacci </a></li>
+<li> <a href="http://ejercicios.local/POO/index.php?method= primos"> Primos </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=factorial"> Factorial </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=potencias"> Potencias </a></li>
-<li> <a href="http://ejercicios.local/POO/index.php?method= primos"> Primos </a></li>
+
 
 </ul>
 
