0fdb9aece383db7d4643cd1e819625d36c909930
JoseLuisHervas <59416429+siniestr0@users.noreply.github.com>
2020-10-08 10:24:54 +0200

    links para xampp

--- POO/views/index.php
+++ POO/views/index.php
@@ -2,11 +2,12 @@
 <ul>
 <li> <a href="http://ejercicios.local/POO/index.php"> index </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=fibonacci"> Fibonacci </a></li>
-<li> <a href="http://ejercicios.local/POO/index.php?method= primos"> Primos </a></li>
+<li> <a href="http://ejercicios.local/POO/index.php?method=primos"> Primos </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=factorial"> Factorial </a></li>
 <li> <a href="http://ejercicios.local/POO/index.php?method=potencias"> Potencias </a></li>
 
-
+<li> Estos son links son del xampp </li>
+<li> <a href="http://ejercicios.local/ejerciciosphp/POO/index.php?method=fibonacci"> Fibonacci </a></li>
 </ul>
 
 
