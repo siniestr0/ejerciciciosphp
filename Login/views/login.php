e44d32fb259a86542d3de476b2d3ef65ba51732b
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-12 14:42:06 +0100

    change method POST->GET

--- Login/views/login.php
+++ Login/views/login.php
@@ -14,12 +14,29 @@
 </head>
 <body>
 
-  <form action="">
+  <form action="" method="GET">
     <label for="">User</label>
-    <input type="text" name="name"> <br>
+    <input type="text" name="user" required> <br> <!-- pongo require para que sea campo obligatorio-->
     <label for="">Password</label>
-    <input type="password" name="password"> <br>
+    <input type="password" name="password" required> <br>
     <input type="submit">
   </form>
+
+
+  <?php
+echo "<br>";
+/* comprobamos si los datos que vamos a introducir existen o no. */
+    if(isset($_GET) && !empty($_GET)){
+        echo "Los datos han sido introducidos.";
+        
+/* ahora vamos a imrpimir los resultados  con el <pre>  se muestra en forma lista */
+/* con el POST no guarda datos, con el GET, si que hace un login, y pide recordar password. */
+        echo "<pre>";
+        print_r($_GET);
+    }
+     else {
+         echo "Campos vacíos.";
+    }    
+?>
 </body>
 </html>
