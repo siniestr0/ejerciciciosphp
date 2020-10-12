5d42adf08adb057e994bd3c6c04948a9edef332e
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-12 14:23:42 +0100

    borrando cosas de rafa para el pull

--- POO/views/index.php
+++ POO/views/index.php
@@ -5,10 +5,7 @@
         <li> <a href="http://ejercicios.local/POO/index.php?method=primos"> Primos </a></li>
         <li> <a href="http://ejercicios.local/POO/index.php?method=factorial"> Factorial </a></li>
         <li> <a href="http://ejercicios.local/POO/index.php?method=potencias"> Potencias </a></li>
-<<<<<<< HEAD
-=======
 
->>>>>>> 51295d6f64dd420d6503a0ff82b1faba83cff882
         Estos son links de Windows
         <li> <a href="http://localhost/ejerciciciosphp/POO/index.php"> index xampp </a></li>
         <li> <a href="http://localhost/ejerciciciosphp/POO/index.php?method=fibonacci"> Fibonacci </a></li>
@@ -16,8 +13,5 @@
         <li> <a href="http://localhost/ejerciciciosphp/POO/index.php?method=factorial"> Factorial </a></li>
         <li> <a href="http://localhost/ejerciciciosphp/POO/index.php?method=potencias2"> Potencias2 </a></li>
     </ul>
-<<<<<<< HEAD
-=======
 
->>>>>>> 51295d6f64dd420d6503a0ff82b1faba83cff882
 </header>
