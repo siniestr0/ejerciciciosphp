6f0be4c6f1ae9f2fa553a1a1856f4feb68dcf515
Jose Luis Hervas <jlcruceras@gmail.com>
2020-10-12 14:31:37 +0100

    links from login

--- Login/views/login.php
+++ Login/views/login.php
@@ -3,13 +3,23 @@
     <li> <a href="http://localhost/ejerciciciosphp/Login/index.php"> index xampp </a></li>
 </ul>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <style></style>
+  <title>Login</title>
+</head>
 <body>
-<form action="">
-    <label for="">User</label>
-    <input type="text" name="user[0]"> <br>
 
+  <form action="">
+    <label for="">User</label>
+    <input type="text" name="name"> <br>
     <label for="">Password</label>
-    <input type="password" name="password[1]"> <br>
+    <input type="password" name="password"> <br>
     <input type="submit">
   </form>
 </body>
+</html>
